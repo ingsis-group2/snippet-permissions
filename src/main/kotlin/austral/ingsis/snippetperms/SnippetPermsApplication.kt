561422c81251c4f97490004d1c7ef2c23d5fdd99
M4rkos2002 <marcos.lambertini@ing.austral.edu.ar>
2024-05-01 15:03:39 -0300

    first commit

---
+++ src/main/kotlin/austral/ingsis/snippetperms/SnippetPermsApplication.kt
@@ -0,0 +1,11 @@
+package austral.ingsis.snippetperms
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SnippetPermsApplication
+
+fun main(args: Array<String>) {
+	runApplication<SnippetPermsApplication>(*args)
+}
---
+++ src/test/kotlin/austral/ingsis/snippetperms/SnippetPermsApplicationTests.kt
@@ -0,0 +1,13 @@
+package austral.ingsis.snippetperms
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SnippetPermsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
