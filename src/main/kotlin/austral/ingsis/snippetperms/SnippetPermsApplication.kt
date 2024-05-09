8c34623f3a610d96b9c63451690f528f88243d9a
MaxiRG <91789878+MaxiRG@users.noreply.github.com>
2024-05-09 16:47:14 -0300

    Update SnippetPermsApplication.kt

--- src/main/kotlin/austral/ingsis/snippetperms/SnippetPermsApplication.kt
+++ src/main/kotlin/austral/ingsis/snippetperms/SnippetPermsApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class SnippetPermsApplication {
-    fun main(args: Array<String>) {
-        runApplication<SnippetPermsApplication>(*args)
-    }
+class SnippetPermsApplication
+
+fun main(args: Array<String>) {
+    runApplication<SnippetPermsApplication>(*args)
 }
