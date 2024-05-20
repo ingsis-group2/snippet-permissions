d1cf997512e86ff6f6cf199e2e10d74f1757279d
MaxiRG <maxiramirezgis@gmail.com>
2024-05-20 15:44:35 -0300

    Fixed Dockerfile.dev

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,7 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     developmentOnly("org.springframework.boot:spring-boot-docker-compose")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
