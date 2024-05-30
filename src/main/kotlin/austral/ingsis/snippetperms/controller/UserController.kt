181fb96b2f004970abd04d576305c80fb38b8d84
M4rkos2002 <marcos.lambertini@ing.austral.edu.ar>
2024-05-30 16:08:12 -0300

    refactor urls

--- src/main/kotlin/austral/ingsis/snippetperms/controller/UserController.kt
+++ src/main/kotlin/austral/ingsis/snippetperms/controller/UserController.kt
@@ -10,8 +10,10 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
 
-@Controller("/permissions/user")
+@Controller
+@RequestMapping("/user")
 class UserController {
     @Autowired
     private lateinit var userService: UserService
