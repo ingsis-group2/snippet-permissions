365b79a08ca3ac4e77f615aeef011cee230704f3
M4rkos2002 <marcos.lambertini@ing.austral.edu.ar>
2024-06-10 00:10:04 -0300

    refactor snippet structure

--- src/main/kotlin/austral/ingsis/snippetperms/model/DTOs.kt
+++ src/main/kotlin/austral/ingsis/snippetperms/model/DTOs.kt
@@ -1,5 +1,7 @@
 package austral.ingsis.snippetperms.model
 
+import java.time.LocalDateTime
+
 data class SnippetCreate(
     val writer: String,
     val name: String,
@@ -7,6 +9,16 @@ data class SnippetCreate(
     val extension: String,
 )
 
-data class SnippetLocation(val id: Long, val container: String)
+data class SnippetDTO(
+    val id: Long,
+    val container: String,
+    val writer: String,
+    val name: String,
+    val language: String,
+    val extension: String,
+    val readers: List<String>,
+    val creationDate: LocalDateTime,
+    val updateDate: LocalDateTime?,
+    )
 
 data class SnippetUpdate(val reader: String)
--- src/main/kotlin/austral/ingsis/snippetperms/service/SnippetService.kt
+++ src/main/kotlin/austral/ingsis/snippetperms/service/SnippetService.kt
@@ -1,26 +1,28 @@
 package austral.ingsis.snippetperms.service
 
+import austral.ingsis.snippetperms.SnippetPermsApplication
 import austral.ingsis.snippetperms.model.Snippet
 import austral.ingsis.snippetperms.model.SnippetCreate
-import austral.ingsis.snippetperms.model.SnippetLocation
+import austral.ingsis.snippetperms.model.SnippetDTO
 import austral.ingsis.snippetperms.repository.SnippetRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
+import java.time.LocalDateTime
 
 @Service
 class SnippetService(
     @Autowired var snippetRepository: SnippetRepository,
+    private val snippetPermsApplication: SnippetPermsApplication,
 ) {
     /*
         Creates a snippet for an existing user
      */
-    fun createSnippet(body: SnippetCreate): ResponseEntity<SnippetLocation> {
-        val container = body.writer.hashCode() % 3
+    fun createSnippet(body: SnippetCreate): ResponseEntity<SnippetDTO> {
         val snippet =
             Snippet().apply {
-                this.container = container.toString()
+                this.container = "snippet"
                 this.writer = body.writer
                 this.language = body.language
                 this.extension = body.extension
@@ -28,16 +30,33 @@ class SnippetService(
 
         val creation = snippetRepository.save(snippet)
         return ResponseEntity.status(HttpStatus.CREATED)
-            .body(SnippetLocation(creation.id, creation.container))
+            .body(this.dto(creation))
     }
 
-    fun getSnippet(snippetId: Long): ResponseEntity<SnippetLocation> {
+    fun getSnippet(snippetId: Long): ResponseEntity<SnippetDTO> {
         return when {
             snippetRepository.existsById(snippetId) -> {
                 val snippet = snippetRepository.findById(snippetId).get()
-                ResponseEntity.ok().body(SnippetLocation(snippet.id, snippet.container))
+                ResponseEntity
+                    .ok()
+                    .body(this.dto(snippet))
             }
             else -> ResponseEntity.notFound().build()
         }
     }
+
+    private fun dto(snippet: Snippet): SnippetDTO {
+        return SnippetDTO(
+            snippet.id,
+            snippet.container,
+            snippet.writer,
+            snippet.name,
+            snippet.language,
+            snippet.extension,
+            snippet.readers.toList(),
+            snippet.creationDate,
+            snippet.updateDate
+        )
+    }
+
 }
