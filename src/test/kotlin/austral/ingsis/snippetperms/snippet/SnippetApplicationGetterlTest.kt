8de61d4de0c069716bd95858af17bf63146f8e9e
M4rkos2002 <marcos.lambertini@ing.austral.edu.ar>
2024-07-02 18:21:58 -0300

    linting

--- src/test/kotlin/austral/ingsis/snippetperms/snippet/SnippetApplicationGetterlTest.kt
+++ src/test/kotlin/austral/ingsis/snippetperms/snippet/SnippetApplicationGetterlTest.kt
@@ -11,7 +11,9 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import jakarta.persistence.EntityManager
 import jakarta.persistence.PersistenceContext
 import org.junit.jupiter.api.AfterEach
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
@@ -22,8 +24,8 @@ import org.springframework.http.MediaType
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.MvcResult
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import org.springframework.transaction.annotation.Transactional
 
 @SpringBootTest(classes = [SnippetTestApplication::class])
@@ -31,7 +33,6 @@ import org.springframework.transaction.annotation.Transactional
 @ActiveProfiles("test")
 @Transactional
 class SnippetApplicationGetterlTest {
-
     @Autowired
     private lateinit var mockMvc: MockMvc
 
@@ -41,14 +42,15 @@ class SnippetApplicationGetterlTest {
     @BeforeEach
     fun beforeEach() {
         repeat(3) {
-            val snippetCreateJson = """
-            {
-                "writer": "testWriter",
-                "name": "testName${it + 1}",
-                "language": "testLanguage",
-                "extension": "txt"
-            }
-            """.trimIndent()
+            val snippetCreateJson =
+                """
+                {
+                    "writer": "testWriter",
+                    "name": "testName${it + 1}",
+                    "language": "testLanguage",
+                    "extension": "txt"
+                }
+                """.trimIndent()
             sendRequestAndReturn(snippetCreateJson, "snippet")
         }
     }
@@ -59,49 +61,59 @@ class SnippetApplicationGetterlTest {
         entityManager.flush() // Forzar el flush para asegurar que se apliquen los cambios
     }
 
-    data class SnippetDTO @JsonCreator constructor(
-        @JsonProperty("id") val id: Long,
-        @JsonProperty("container") val container: String,
-        @JsonProperty("writer") val writer: String,
-        @JsonProperty("name") val name: String,
-        @JsonProperty("language") val language: String,
-        @JsonProperty("extension") val extension: String,
-        @JsonProperty("readers") val readers: List<String>,
-        @JsonProperty("creationDate") val creationDate: String,
-        @JsonProperty("updateDate") val updateDate: String?
-    )
+    data class SnippetDTO
+        @JsonCreator
+        constructor(
+            @JsonProperty("id") val id: Long,
+            @JsonProperty("container") val container: String,
+            @JsonProperty("writer") val writer: String,
+            @JsonProperty("name") val name: String,
+            @JsonProperty("language") val language: String,
+            @JsonProperty("extension") val extension: String,
+            @JsonProperty("readers") val readers: List<String>,
+            @JsonProperty("creationDate") val creationDate: String,
+            @JsonProperty("updateDate") val updateDate: String?,
+        )
 
-    private fun sendRequestAndReturn(snippetCreateJson: String, url: String): MvcResult {
+    private fun sendRequestAndReturn(
+        snippetCreateJson: String,
+        url: String,
+    ): MvcResult {
         return mockMvc.perform(
             post("http://localhost:8080/$url")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(snippetCreateJson)
+                .content(snippetCreateJson),
         ).andReturn()
     }
 
     private fun sendGetRequestAndReturn(url: String): MvcResult {
         return mockMvc.perform(
-                get("http://localhost:8080/$url")
-                    .contentType(MediaType.APPLICATION_JSON)
-                ).andReturn()
+            get("http://localhost:8080/$url")
+                .contentType(MediaType.APPLICATION_JSON),
+        ).andReturn()
     }
 
-    private fun addReaderIntoSnippet(snippetId: Long, writerId: String, readerId: String) {
-        val readerForm = NewReaderForm(
-            snippetId,
-            writerId,
-            readerId
-        )
+    private fun addReaderIntoSnippet(
+        snippetId: Long,
+        writerId: String,
+        readerId: String,
+    ) {
+        val readerForm =
+            NewReaderForm(
+                snippetId,
+                writerId,
+                readerId,
+            )
         val objectMapper = ObjectMapper()
         val jsonContent = objectMapper.writeValueAsString(readerForm)
         this.sendRequestAndReturn(jsonContent, "/snippet/addReader")
     }
 
     private fun getSnippetFormDB(snippetId: Long): Snippet {
-        val second_query = entityManager.createQuery("SELECT s FROM Snippet s WHERE s.id = :id", Snippet::class.java)
-        second_query.maxResults = 1
-        second_query.setParameter("id", snippetId)
-        return second_query.resultList.firstOrNull() as Snippet
+        val secondQuery = entityManager.createQuery("SELECT s FROM Snippet s WHERE s.id = :id", Snippet::class.java)
+        secondQuery.maxResults = 1
+        secondQuery.setParameter("id", snippetId)
+        return secondQuery.resultList.firstOrNull() as Snippet
     }
 
     @Test
@@ -129,7 +141,7 @@ class SnippetApplicationGetterlTest {
     fun `should fail get a snippet because does not exist on database`() {
         val maxIdQuery = entityManager.createQuery("SELECT MAX(s.id) FROM Snippet s")
         val maxId = maxIdQuery.singleResult as Long
-        val nonExistentId = maxId + 1000L  //for sure id does not exists
+        val nonExistentId = maxId + 1000L // for sure id does not exists
 
         val result = this.sendGetRequestAndReturn("snippet/$nonExistentId")
         val content = result.response.contentAsString
@@ -139,11 +151,12 @@ class SnippetApplicationGetterlTest {
 
     @Test
     fun `should send a list of snippets by writer test`() {
-        val getterForm = GetterForm(
-            userId = "testWriter",
-            page = 0,
-            size = 10
-        )
+        val getterForm =
+            GetterForm(
+                userId = "testWriter",
+                page = 0,
+                size = 10,
+            )
         val objectMapper = ObjectMapper()
         val jsonContent = objectMapper.writeValueAsString(getterForm)
 
@@ -159,17 +172,18 @@ class SnippetApplicationGetterlTest {
         val query = entityManager.createQuery("SELECT s.id FROM Snippet s", Long::class.java)
         val snippetIds: List<Long> = query.resultList
         assertEquals(3, snippetIds.size)
-        val first_Id = snippetIds[0]
-        val second_Id = snippetIds[1]
+        val firstId = snippetIds[0]
+        val secondId = snippetIds[1]
 
-        this.addReaderIntoSnippet(first_Id, "testWriter", "testReader")
-        this.addReaderIntoSnippet(second_Id, "testWriter", "testReader")
+        this.addReaderIntoSnippet(firstId, "testWriter", "testReader")
+        this.addReaderIntoSnippet(secondId, "testWriter", "testReader")
 
-        val getterForm = GetterForm(
-            userId = "testReader",
-            page = 0,
-            size = 10
-        )
+        val getterForm =
+            GetterForm(
+                userId = "testReader",
+                page = 0,
+                size = 10,
+            )
 
         val objectMapper = ObjectMapper()
         val jsonContent = objectMapper.writeValueAsString(getterForm)
@@ -179,17 +193,18 @@ class SnippetApplicationGetterlTest {
         val snippets: List<SnippetDTO> = objectMapper.readValue(content, object : TypeReference<List<SnippetDTO>>() {})
         assertNotNull(snippets)
 
-        val first_snippet = this.getSnippetFormDB(first_Id)
-        val second_snippet = this.getSnippetFormDB(second_Id)
-        val first_snippet_readers = first_snippet.readers
-        val second_snippet_readers = second_snippet.readers
-        assertTrue(first_snippet_readers.contains("testReader"))
-        assertTrue(second_snippet_readers.contains("testReader"))
+        val firstSnippet = this.getSnippetFormDB(firstId)
+        val secondSnippet = this.getSnippetFormDB(secondId)
+        val firstSnippetReaders = firstSnippet.readers
+        val secondSnippetReaders = secondSnippet.readers
+        assertTrue(firstSnippetReaders.contains("testReader"))
+        assertTrue(secondSnippetReaders.contains("testReader"))
     }
 
     @Test
     fun `should send a list of snippets by reader and writer test`() {
-        val snippetCreateJson = """
+        val snippetCreateJson =
+            """
             {
                 "writer": "anotherTestWriter",
                 "name": "AnotherName",
@@ -198,38 +213,40 @@ class SnippetApplicationGetterlTest {
             }
             """.trimIndent()
         val response = sendRequestAndReturn(snippetCreateJson, "snippet")
-        val new_snippet_content = response.response.contentAsString
+        val newSnippetContent = response.response.contentAsString
         val objectMapper = ObjectMapper()
-        val new_snippet = objectMapper.readValue(new_snippet_content, SnippetDTO::class.java)
-        this.addReaderIntoSnippet(new_snippet.id, "anotherTestWriter", "testWriter")
+        val newSnippet = objectMapper.readValue(newSnippetContent, SnippetDTO::class.java)
+        this.addReaderIntoSnippet(newSnippet.id, "anotherTestWriter", "testWriter")
 
-        val getterForm = GetterForm(
-            userId = "testWriter",
-            page = 0,
-            size = 10
-        )
+        val getterForm =
+            GetterForm(
+                userId = "testWriter",
+                page = 0,
+                size = 10,
+            )
         val jsonContent = objectMapper.writeValueAsString(getterForm)
         val result = this.sendRequestAndReturn(jsonContent, "snippet/byReaderAndWriter")
         val content = result.response.contentAsString
         val snippets: List<SnippetDTO> = objectMapper.readValue(content, object : TypeReference<List<SnippetDTO>>() {})
         assertNotNull(snippets)
-        assertEquals(snippets.size, 4)  //three that wrote before the test and one that can read from another writer
+        assertEquals(snippets.size, 4) // three that wrote before the test and one that can read from another writer
         for (snippet in snippets) {
-            if (snippet.id == new_snippet.id) {
+            if (snippet.id == newSnippet.id) {
                 assertTrue(snippet.readers.contains("testWriter"))
             } else {
-              assertEquals(snippet.writer, "testWriter")
+                assertEquals(snippet.writer, "testWriter")
             }
         }
     }
 
     @Test
     fun `should fail sending a list of snippets by writer who has no snippets test`() {
-        val getterForm = GetterForm(
-            userId = "WriterWhoHasNoSnippetsWritten",
-            page = 0,
-            size = 10
-        )
+        val getterForm =
+            GetterForm(
+                userId = "WriterWhoHasNoSnippetsWritten",
+                page = 0,
+                size = 10,
+            )
         val objectMapper = ObjectMapper()
         val jsonContent = objectMapper.writeValueAsString(getterForm)
         val result = this.sendRequestAndReturn(jsonContent, "snippet/byReaderAndWriter")
